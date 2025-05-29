ed6c85dbcdbf73345337428845d8591cd20ff130
itsxHrisi <122966420+itsxHrisi@users.noreply.github.com>
2025-05-30 00:07:10 +0200

    cambios rutas

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 //Rutas para trabajar en deploiment
 export const environment = {
     production: false,
-    authUrl: 'http://localhost:8091/auth',
-    apiUrl: 'http://localhost:8091/api'
+    authUrl: 'http://135.225.90.208:8091/auth',
+    apiUrl: 'http://135.225.90.208:8091/api'
 };
